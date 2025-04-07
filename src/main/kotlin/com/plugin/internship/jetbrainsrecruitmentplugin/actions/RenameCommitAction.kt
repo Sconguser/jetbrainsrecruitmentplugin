98bd0bca361a9257c0e8bdc4b193871c984d0b64
Sconguser <scongi.30@gmail.com>
2025-04-07 11:14:54 +0200

    action perfomed fix after update method change

--- src/main/kotlin/com/plugin/internship/jetbrainsrecruitmentplugin/actions/RenameCommitAction.kt
+++ src/main/kotlin/com/plugin/internship/jetbrainsrecruitmentplugin/actions/RenameCommitAction.kt
@@ -2,13 +2,11 @@ package com.plugin.internship.jetbrainsrecruitmentplugin.actions
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.ui.NonEmptyInputValidator
-import git4idea.GitUtil
 import git4idea.GitVcs
 import git4idea.branch.GitBranchUtil
 import git4idea.checkin.GitCheckinEnvironment
@@ -33,21 +31,18 @@ class RenameCommitAction:DumbAwareAction() {
         ApplicationManager.getApplication()
             .executeOnPooledThread {
                 try {
-                    val file = event.getData(CommonDataKeys.VIRTUAL_FILE)
-                        ?: throw RuntimeException("File is null")
                     val gitVcs = GitVcs.getInstance(project)
                     val gitCheckinEnvironment =
                         gitVcs.checkinEnvironment as GitCheckinEnvironment? ?: return@executeOnPooledThread
                     if (!gitVcs.isCommitActionDisabled && gitCheckinEnvironment.isAmendCommitSupported()) {
-                        val repository: GitRepository? =
-                            GitUtil.getRepositoryManager(project).getRepositoryForFileQuick(file)
-                        if (repository != null) {
+                        val gitRepository = GitBranchUtil.guessWidgetRepository(project, event.dataContext)
+                        if (gitRepository != null) {
                             val handler =
-                                gitLineHandler(project, repository, newMessage)
+                                gitLineHandler(project, gitRepository, newMessage)
                             /// TODO: what if something is staged
                             val command = Git.getInstance().runCommand(handler)
                             command.throwOnError()
-                            repository.update()
+                            gitRepository.update()
                         }
                     }
                 } catch (e: Exception) {
